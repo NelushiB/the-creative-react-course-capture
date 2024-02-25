d23b8eec6e4ed5f772c5778be1d5391c836f8e18
Nelushi Bamunusinghe <nelushib@hotmail.com>
2024-02-25 13:32:36 +0100

    3. Service & Faq section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 // Page Components
-import AboutSection from "../components/AboutSection"
+import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return (
-    <AboutSection />
-  )
-}
+   return (
+      <>
+         <AboutSection />
+         <ServiceSection />
+         <FaqSection />
+      </>
+   )
+};
 
 export default AboutUs;
