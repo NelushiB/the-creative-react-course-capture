8cae73c9b4302e10552c95c0fdc5b0c41f6fd485
Nelushi Bamunusinghe <nelushib@hotmail.com>
2024-02-25 23:22:54 +0100

    6. Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // Import Global Style
 import GlobalStyle from "./components/GlobalStyle";
 
 // Import Pages
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 
 function App() {
+
    return (
       <div className="App">
          <GlobalStyle />
-         <AboutUs />
+         <Nav />
+         <Routes>
+            <Route path="/" element={<AboutUs />} />
+            <Route path="/work" element={<OurWork />} />
+            <Route path="/contact" element={<ContactUs />} />
+         </Routes>
+
       </div>
    );
 }
