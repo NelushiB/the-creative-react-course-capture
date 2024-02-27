401e2e9cf78e814e2b8dde602e0944e89f089f21
Nelushi Bamunusinghe <nelushib@hotmail.com>
2024-02-27 21:46:37 +0100

    11. Toggle Faq Section

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,43 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+//Style
 import { styled } from 'styled-components';
 import { About } from '../styles';
+//Reusable component
+import Toggle from './Toggle';
+//Animation
+import { LayoutGroup } from 'framer-motion';
 
 const FaqSection = () => {
+
    return (
       <Faq>
          <h2>Any Questions <span>FAQ</span></h2>
-         <div className="question">
-            <h4>How Do I Start?</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet.</p>
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+         <LayoutGroup>
+         <Toggle title="How Do I Start?">
+            <div className="question">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>Daily Schedule</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet.</p>
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+         </Toggle>
+         <Toggle title="Daily Schedule">
+            <div className="question">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>Different Payment Methods</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet.</p>
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+         </Toggle>
+         <Toggle title="Different Payment Methods">
+            <div className="question">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>What product do you offer?</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet.</p>
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+         </Toggle>
+         <Toggle title="What product do you offer?">
+            <div className="question">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+               </div>
             </div>
-            <div className="faq-line"></div>
-         </div>
+         </Toggle>
+         </LayoutGroup>
       </Faq>
    )
 };
