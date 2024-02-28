0cf9fa581bf4f73ca50376c2246ea9be9c0f1e68
Nelushi Bamunusinghe <nelushib@hotmail.com>
2024-02-28 22:15:35 +0100

    15. Nav Line

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,68 +1,102 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
+   const { pathname } = useLocation();
    return (
       <StyledNav>
-         <h1><Link id='logo' to="/">Capture</Link></h1>
+         <h1>
+            <Link id="logo" to="/">
+               Capture
+            </Link>
+         </h1>
          <ul>
             <li>
                <Link to="/">1. About Us</Link>
+               <Line
+                  transition={{ duration: 0.5 }}
+                  initial={{ width: "0%" }}
+                  animate={{ width: pathname === "/" ? "50%" : "0%"}}
+               />
             </li>
             <li>
                <Link to="/work">2. Our Work</Link>
+               <Line
+                  transition={{ duration: 0.5 }}
+                  initial={{ width: "0%" }}
+                  animate={{ width: pathname === "/work" ? "50%" : "0%"}}
+               />
             </li>
             <li>
                <Link to="/contact">3. Contact Us</Link>
+               <Line
+                  transition={{ duration: 0.5 }}
+                  initial={{ width: "0%" }}
+                  animate={{ width: pathname === "/contact" ? "50%" : "0%"}}
+               />
             </li>
          </ul>
       </StyledNav>
-   )
+   );
 };
 
 // Creare sempre la funzione di styled fuori dal componente principale per evitare che venga reinderizzato quando viene reinderizzato il Nav
 const StyledNav = styled.nav`
-   min-height: 10vh;
-   display: flex;
-   margin: auto;
-   justify-content: space-between;
-   align-items: center;
-   padding: 1rem 10rem;
-   background: #282828;
-   position: sticky;
-   top: 0;
-   z-index: 10;
-   a {
-      color: white;
-      text-decoration: none;
-   }
-   ul {
-      display: flex;
-      list-style: none;
-   }
-   #logo {
-      font-size: 1.5rem;
-      font-family: "Lobster", cursive;
-      font-weight: lighter;
-   }
-   li{
-      padding-left: 5rem;
-      position: relative;
-   }
-   @media (max-width: 1300px) {
-      flex-direction: column;
-      padding: 1rem;
-      ul {
-         padding: 2rem;
-         justify-content: space-around;
-         width: 100%;
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 5rem;
+    position: relative;
+  }
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 1rem;
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
 
-         li {
-            padding: 0;
-         }
+      li {
+        padding: 0;
       }
-   }
+    }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 5%;
+  position: absolute;
+  bottom: -80%;
+  left: 50%;
+  @media (max-width: 1300px) {
+     left: 30%;
+  }
 `;
 
 export default Nav;
